55092e5c0d70b41ac447ae0e40f3b93a48e04cbb
Aditya Chavan <adityachavan198@gmail.com>
2023-11-17 01:29:56 -0500

    Added Frontend using Streamlit and updated Readme

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # gen-ai-travel-planner
+
+Travel Itinerary Generator
+This is a simple Travel Itinerary Generator using Streamlit and the Google PaLM model for text generation. The app takes user input, such as the city to visit, the number of days for the trip, and user preferences for activities, and generates a personalized itinerary.
+
+Prerequisites
+Before running the app, make sure you have the required dependencies installed. You can install them using the following command:
+
+
+pip install streamlit google.generativeai
+Getting Started
+Clone this repository to your local machine:
+
+git clone https://github.com/adityachavan198/gen-ai-travel-planner
+Change to the project directory:
+
+cd gen-ai-travel-planner
+Set up your Palm API key:
+Obtain an API key from Google.
+Set the API key as an environment variable. You can do this in your terminal:
+
+export PALM_API_KEY="your-api-key"
+Run the Streamlit app:
+
+streamlit run app.py
+Open your web browser and go to http://localhost:8501 to access the app.
+Usage
+Enter the city you're visiting and the number of days for your trip.
+Check the preferences for activities you're interested in.
+Click the "Generate Itinerary" button to get a personalized itinerary.
+Contributing
+Feel free to contribute to the development of this project by opening issues or submitting pull requests.
--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import pprint
 import google.generativeai as palm
 import os
@@ -7,15 +8,40 @@ models = [m for m in palm.list_models() if 'generateText' in m.supported_generat
 model = models[0].name
 print(model)
 
-prompt = """
-You are an travel expert.
+# Streamlit app title and user input
+st.title("Travel Itinerary Generator")
 
-Give me an itenary for 3 days in New York City for someone who looks art, music, culture, historic places.
+city = st.text_input("Enter the city you're visiting:")
+days = st.number_input("Enter the number of days for your trip:", min_value=1, max_value=30)
 
+# User preferences checkboxes
+art = st.checkbox("Art")
+museums = st.checkbox("Museums")
+outdoor = st.checkbox("Outdoor Activities")
+indoor = st.checkbox("Indoor Activities")
+kids_friendly = st.checkbox("Good for Kids")
+young_people = st.checkbox("Good for Young People")
 
-"""
+# Generate itinerary button
+if st.button("Generate Itinerary"):
+    # Create a prompt based on user input
+    
+    prompt = f"You are an travel expert. Give me an itenary for {city}, for {days} days, I like to"
+    if art:
+        prompt += " explore art,"
+    if museums:
+        prompt += " visit museums,"
+    if outdoor:
+        prompt += " engage in outdoor activities,"
+    if indoor:
+        prompt += " explore indoor activities,"
+    if kids_friendly:
+        prompt += " find places suitable for kids,"
+    if young_people:
+        prompt += " discover places suitable for young people,"
 
-completion = palm.generate_text(
+    # Call the OpenAI API
+    completion = palm.generate_text(
     model=model,
     prompt=prompt,
     temperature=0,
@@ -23,4 +49,7 @@ completion = palm.generate_text(
     max_output_tokens=800,
 )
 
-print(completion.result)
+    # Extract and display the generated itinerary
+    itinerary = completion.result.strip()
+    st.subheader("Your Personalized Itinerary:")
+    st.write(itinerary)
